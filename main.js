3a191f70282bec4ac7ae3ba8b998dd53529f2b80
sayan-1210 <sayanpaul.8924@gmail.com>
2023-11-16 22:55:43 +0530

    save

--- main.js
+++ main.js
@@ -1,1 +1,83 @@
-console.log('Hello World')
+// //*****log statement *********
+// console.log('Hello World')
+
+// //******variables********
+// let age=20     //can be changed in future
+// console.log(age)
+
+// const salary=8000      //can't be changed in future
+// console.log(salary)
+
+// //*************DATA TYPES****************
+// //primitive
+// //string 
+// const name="Sayan"
+
+// //number(int and float)
+// const total=0
+
+// //bool
+// const isNewUser=false
+
+// //undefined
+// let res
+// const result=undefined
+// console.log(res,result)
+
+// //null
+// const data=null
+
+// //unprimitive
+// //object
+// const person={
+//     'firstName': 'Bruce',
+//     'lastName': 'Wayne',
+//     'age': 30
+// }
+
+// console.log(person.firstName)
+
+// //array
+// const oddNumbers = [1,3,5,7,9]
+// console.log(oddNumbers[0])
+
+
+// //**********OPERATORS**************
+// //assignment
+// let x=10
+// let y=5
+
+// //arithmetic
+// console.log(x+y)
+// console.log(x-y)
+// console.log(x*y)
+// console.log(x/y)
+// console.log(x%y)
+// console.log(++x)
+// console.log(--y)
+
+// //comparison
+// console.log(x==y)
+// console.log(x!=y)
+// console.log(x===y)
+// console.log(x!==y)
+// console.log(x>y)
+// console.log(x>=y)
+// console.log(x<y)
+// console.log(x<=y)
+
+// //logical
+// const isValidNumber=x>8 && 8>y      //logical AND
+// console.log(isValidNumber)
+
+// const isValidNumber=x>20 || 8>y      //logical OR
+// console.log(isValidNumber)
+
+// const isValid = true
+// console.log(!isValid)                   //logical NOT
+
+// console.log('Bruce '+'Wayne')            //string operator
+
+// const isEven = 10%2 === 0? true:false        //ternary operator
+// console.log(isEven)
+
